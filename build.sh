d908bbd805064d2c1dfc06e48ce549f20ccf648a
Bryan Klimt <bklimt@gmail.com>
2012-06-16 16:44:42 -0700

    Factor out the Graphic stuff.

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 set -x
 coffee --join js/knitplot.js --compile \
   coffee/Library.coffee \
-  coffee/ChartParser.coffee \
   coffee/Pattern.coffee \
+  coffee/ChartParser.coffee \
+  coffee/Graphic.coffee \
   coffee/NotificationView.coffee \
   coffee/ErrorView.coffee \
   coffee/PatternEditView.coffee \
